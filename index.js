4809a60cbedc0dfcf8ca9d22436380053decbee6
karthik <karthikeyavempala>
2025-03-16 20:47:09 +0530

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express= require('express');
-
+const dotenv=require('dotenv');
 
 const app=express();
 
+dotenv.config();
 
 app.get('/home',(req,res)=>{
     res.json({message:'OK'})
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server Started ")
 })
